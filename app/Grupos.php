c92b001ff7152a40fd3d4ad64920d56da859075c
Luis Santillan <santillan.itc@gmail.com>
2017-05-25 00:42:29 -0600

    cambio de M minuscula

--- app/Grupos.php
+++ app/Grupos.php
@@ -13,7 +13,7 @@ class Grupos extends Model
     {
         return $this->belongsToMany('App\Materiales', 'detalle_cotizaciones', 'id_grupo', 'id_material')
          ->withPivot('cantidad_gpo', 'id_cotizacion', 'precio', 'cantidad')
-         ->selectRaw('detalle_cotizaciones.cantidad*detalle_cotizaciones.precio AS cant_precio, Materiales.*')
+         ->selectRaw('detalle_cotizaciones.cantidad*detalle_cotizaciones.precio AS cant_precio, materiales.*')
          ->withTimestamps();
     }
 
